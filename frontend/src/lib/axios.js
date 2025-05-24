76db17fb3e9edb6f25db1f8d97d1f69a73ca568d
AJAY-BHATT <138021749+AJAY-BHATT@users.noreply.github.com>
2025-05-24 12:08:58 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://mern-chat-vc.onrender.com/api" :"/api"
+const BASE_URL = import.meta.env.MODE === "development" ? "https://mern-chat-vc.onrender.com" :"/api"
 
 // export const axiosInstance = axios.create({
 //     baseURL:"http://localhost:5000/api",
